32f553c64e364dff52aae898999a4e608ee5eeee
Jean Ruffato <jean.ruffato@gmail.com>
2018-09-24 19:12:15 -0300

    Remoção de modelos do Admin Django

--- dailyapi/admin.py
+++ dailyapi/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Atividades)
-admin.site.register(models.Projetos)
+admin.site.register(models.Apontamento)
 admin.site.register(models.Perfil)
 
